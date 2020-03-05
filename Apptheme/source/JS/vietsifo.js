f0664ba6c37d0b228d9d3e7215b13d1891d91964
luffy <gb8296@gmail.com>
2020-03-05 15:40:32 +0700

    Initial commit

---
+++ Apptheme/source/JS/vietsifo.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $(".page_header .btn-expand").on('click', btnExpandClick);
+
+    $(".header_slide").bxSlider({
+        loop: false,
+        auto: 0,
+        nextText: '',
+        prevText: '',
+        pagerSelector: '#feature-pager',
+        swipeThreshold: 5
+
+    });
+
+    $(".cmt_slide").bxSlider({
+        loop: true,
+        auto: 1,
+        nextText: '',
+        prevText: '',
+        pagerSelector: '#cmt-pager',
+        swipeThreshold: 55
+
+    });
+});
+
+function btnExpandClick(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    expandNav();
+}
+
+function expandNav() {
+    $('.page_header .nav').slideToggle(200);
+    $('.btn-expand .fa-bars').toggle();
+    $('.btn-expand .fa-times').toggle();
+}
